15709daf8c48b6209e76faea560db3a7aed77be2
ggosdinski <gianfranco.gosdinski@icloud.com>
2025-03-21 18:28:24 -0500

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      unoptimized: true, // Esto desactiva la optimización automática de imágenes
+    },
+  };
+  
+  module.exports = nextConfig;
+  
