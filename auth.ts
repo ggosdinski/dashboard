751fd54f48dd9045473d130b511db6c98a316d42
ggosdinski <gianfranco.gosdinski@icloud.com>
2025-03-22 17:54:24 -0500

    Replace bcrypt with bcryptjs for Vercel compatibility

--- auth.ts
+++ auth.ts
@@ -4,7 +4,8 @@ import { authConfig } from './auth.config';
 import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
+
 
 async function getUser(email: string): Promise<User | undefined> {
     try {
@@ -30,7 +31,7 @@ export const { auth, signIn, signOut } = NextAuth({
             const { email, password } = parsedCredentials.data;
             const user = await getUser(email);
             if (!user) return null;
-            const passwordsMatch = await bcrypt.compare(password, user.password);
+            const passwordsMatch = await bcryptjs.compare(password, user.password);
             if (passwordsMatch) return user;
           }
 
